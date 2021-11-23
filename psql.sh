9ac3a6e021fea9aa2d1f225e3debd65114c0a848
em <eugen.mayer@kontextwork.de>
2021-11-23 10:10:58 +0100

    verify cert on connection

--- psql.sh
+++ psql.sh
@@ -2,4 +2,4 @@
 
 source .env
 NETWORK=docker-database-cluster_dbs
-docker run --rm --link postgres:$PG_DOMAIN -e PGREQUIRESSL=1 -e PGPASSWORD=${POSTGRES_PASSWORD} -it --network $NETWORK bitnami/postgresql:latest psql -h $PG_DOMAIN -U postgres 
+docker run --rm --link postgres:$PG_DOMAIN -e PGSSLROOTCERT=/etc/ssl/certs/ca-certificates.crt -e PGREQUIRESSL=1 -e PGSSLMODE=verify-full -e PGPASSWORD=${POSTGRES_PASSWORD} -it --network $NETWORK bitnami/postgresql:latest psql -h $PG_DOMAIN -U postgres 
