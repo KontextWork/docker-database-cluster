bd08609c93728bec2c68e6e1ac1c2855ac77d761
em <eugen.mayer@kontextwork.de>
2021-11-24 15:16:35 +0100

    use autocomit

--- pdbdrop.sh
+++ pdbdrop.sh
@@ -18,6 +18,6 @@ source .env
 NETWORK=database-cluster_dbs
 CMD="docker run --rm --link postgres:$PG_DOMAIN -e PGREQUIRESSL=1 -e PGPASSWORD=${POSTGRES_PASSWORD} -it --network $NETWORK bitnami/postgresql:latest psql -h $PG_DOMAIN -U postgres -c"
 
-$CMD "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DATABASE';drop database $DATABASE;"
+$CMD "\set AUTOCOMMIT on;SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DATABASE';drop database $DATABASE;"
 
 echo "- Dropped database $DATABASE"
--- punprov.sh
+++ punprov.sh
@@ -24,7 +24,7 @@ source .env
 NETWORK=database-cluster_dbs
 CMD="docker run --rm --link postgres:$PG_DOMAIN -e PGREQUIRESSL=1 -e PGPASSWORD=${POSTGRES_PASSWORD} -it --network $NETWORK bitnami/postgresql:latest psql -h $PG_DOMAIN -U postgres -c"
 
-$CMD "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DATABASE';drop database $DATABASE;"
+$CMD "\set AUTOCOMMIT on;SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DATABASE';drop database $DATABASE;"
 $CMD "DROP USER ${USER}"
 
 echo "- Removed user $USER"
