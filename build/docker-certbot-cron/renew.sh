b2374f970b5826b6f1c117e9a8aac65c03ad7a5f
root <root@docker-util-services.host.kontextwork.net>
2022-09-07 05:58:48 +0000

    fix cron task scheduler

--- build/docker-certbot-cron/renew.sh
+++ build/docker-certbot-cron/renew.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cd /workdir
-echo "Renewing Let's Encrypt Certificates... (`date`)"
-CONTAINER_NAME=$COMPOSE_PROJECT_NAME-$CERTBOT_SERVICE_NAME
+echo "Checking and renewing Let's Encrypt Certificates if needed... (`date`)"
+CONTAINER_NAME=${COMPOSE_PROJECT_NAME}_${CERTBOT_SERVICE_NAME}
 docker start $CONTAINER_NAME
 docker logs $CONTAINER_NAME
