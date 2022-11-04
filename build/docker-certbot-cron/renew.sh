4babc5aeb119705f1aae3fc06aad2152e4e0bb26
Eugen Mayer <136934+EugenMayer@users.noreply.github.com>
2022-11-04 09:26:13 +0100

    Show only most recent logs

--- build/docker-certbot-cron/renew.sh
+++ build/docker-certbot-cron/renew.sh
@@ -3,4 +3,5 @@
 echo "Checking and renewing Let's Encrypt Certificates if needed... (`date`)"
 CONTAINER_NAME=${COMPOSE_PROJECT_NAME}_${CERTBOT_SERVICE_NAME}
 docker start $CONTAINER_NAME
-docker logs $CONTAINER_NAME
+# show most recent logs
+docker $CONTAINER_NAME --since 5m logs
